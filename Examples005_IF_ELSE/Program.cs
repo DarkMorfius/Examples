16eb4322f5b43a72affe511ce278429a668b5df4
Morf Brooks <morfius_97_97@mail.ru>
2023-02-19 17:41:45 +0600

    Add Examples005_IF_ELSE

---
+++ Examples005_IF_ELSE/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write(" Введите имя пользователя: ");
+
+String username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+
+    Console.WriteLine("Yes. This is Masha!");
+
+}
+else
+{
+
+    Console.Write(" Hello, ");
+    Console.WriteLine(username);
+    {
+        
+    }
+}
